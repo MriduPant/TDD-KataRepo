506772aaca5839c095897768e1f9025ad5864479
MriduPant <mdu.atZenith@gmail.com>
2011-12-01 11:34:35 +0530

    Added Kata

---
+++ TDD-Kata/TDD-Kata/StringCalculator/StringCalculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Kata
+    {
+    public class Problem
+        {
+
+
+        static string[] split = new string[5];
+        static char[] del = { '/', ':', ',', '\n', '.', '*', ' ' };
+        static string trimChar = "\\";
+        public static int Add(string numbers)
+            {
+            return (numbers == string.Empty ? 0 : Add1(numbers));
+            }
+
+        private static int Add1(string numbers)
+            {
+            return (Problem.getDelimiter(numbers) ? Add2(numbers) : (Convert.ToInt32(numbers)));
+            }
+        private static bool getDelimiter(string numbers)
+            {
+            bool find = false;
+            if (numbers.Contains("-"))
+                {
+                ValidateInput(numbers);
+                }
+            else if (!(numbers.IndexOfAny(del) == -1))
+                {
+                find = true;
+                }
+            return find;
+            }
+        private static void ValidateInput(string numbers)
+            {
+            if (Convert.ToInt32(numbers) < 0)
+                throw new ArgumentException(string.Format("Value cannot be Negative", numbers));
+            }
+
+        private static int Add2(string numbers)
+            {
+            int add = 0;
+            int count = 0;
+            string num = string.Empty;
+            if (numbers.StartsWith(trimChar))
+                {
+                numbers = numbers.TrimStart(trimChar.ToCharArray());
+                }
+            split = numbers.Split(del);
+            for (int i = 0; i < split.Length; i++)
+                {
+                if (split[i] == "")
+                    {
+                    numbers = Convert.ToString(0);
+                    add = add + Convert.ToInt32(numbers);
+                    }
+                else
+                    {
+                    numbers = split[i];
+                    add = add + Convert.ToInt32(numbers);
+                    }
+                }
+            return add;
+            }
+
+
+        }
+    }
---
+++ TDD-Kata/TDD-Kata/StringCalculator/TestStringCalculator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDD_Kata
+    {
+    [TestFixture]
+    public class Runner
+        {
+
+        [Test]
+        public void Add_WhenGivenNoNumbers_ReturnsZero()
+            {
+            var result = Problem.Add("");
+
+            Assert.That(result, Is.EqualTo(0));
+            }
+
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("10")]
+        [Test]
+        public void Add_WhenGivenOneNumber_ReturnsThatNumber(string value)
+            {
+            var result = Problem.Add(value);
+            Assert.That(result, Is.EqualTo(Convert.ToInt32(value)), "");
+            }
+
+        [TestCase("6,7", 13)]
+        [TestCase("1:2", 3)]
+        [TestCase("1/10", 11)]
+        [TestCase("1//10", 11)]
+        [TestCase("1\n2", 3)]
+        [TestCase("3\n7", 10)]
+        [Test]
+        public void Add_WhenGivenTwoNumbers_AddNumbersTogether(string twoNumbers, int expected)
+            {
+            var result = Problem.Add(twoNumbers);
+            Assert.That(result, Is.EqualTo(expected));
+            }
+
+
+        [TestCase("1\n2\n3", 6)]
+        [TestCase("1:2:3", 6)]
+        [TestCase("1,2,3", 6)]
+        [TestCase("1 2 3", 6)]
+        [TestCase("1/2/10", 13)]
+        [TestCase("1//2//10", 13)]
+        [TestCase("1:2,4", 7)]
+        [TestCase("1\n2:3", 6)]
+        [TestCase("//1\n2:5", 8)]
+        [TestCase("//:1,2:8", 11)]
+        [Test]
+        public void Add_WhenGivenNewLinesBetweenNumbers_TreatsNewLineAsDelimiter(string value, int expected)
+            {
+            var result = Problem.Add(value);
+            Assert.That(result, Is.EqualTo(expected));
+            }
+
+        [TestCase("\\:\n1:3", 4)]
+        [TestCase("\\:\n1,4", 5)]
+        [Test]
+        public void Add_WhenGivenTwoSlashesAtBeginning_UsesDelimiterAfterSlashesInsteadOfComma(string value, int expected)
+            {
+            var result = Problem.Add(value);
+            Assert.That(result, Is.EqualTo(expected));
+            }
+
+        [Test]
+        [TestCase("-1")]
+
+        public void Add_WhenGivenNegativeNumbers_ThrowsExceptionSayingNegativesArentAllowed(string numbers)
+            {
+            Assert.Throws<ArgumentException>(() => Problem.Add(numbers));
+            }
+        }
+    }
