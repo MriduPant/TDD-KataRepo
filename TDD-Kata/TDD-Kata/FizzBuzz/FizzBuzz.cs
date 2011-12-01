81988cb26904d433c25d50b25c550e796b1e9e63
MriduPant <mdu.atZenith@gmail.com>
2011-12-01 11:48:40 +0530

    Added FizzBuzz

---
+++ TDD-Kata/TDD-Kata/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Kata.FizzBuzz
+    {
+    public class Problem
+        {
+        public static string FizzBuzz(int input)
+            {
+            return EvaluateInput(input);
+            }
+
+        private static string EvaluateInput(int input)
+            {
+            return CheckCondition(input);
+            }
+
+        private static string CheckCondition(int input)
+            {
+            string result = string.Empty;
+            if (!(TestBoth(input) || TestWithThree(input) || TestWithFive(input)))
+                {
+                result = input.ToString();
+                }
+            else if (TestBoth(input))
+                {
+                result = "FizzBuzz";
+                }
+            else if (TestWithThree(input))
+                {
+                result = "Fizz";
+                }
+            else if (TestWithFive(input))
+                {
+                result = "Buzz";
+                }
+            return result;
+            }
+
+        private static bool TestWithFive(int input)
+            {
+            return (input % 5 == 0);
+            }
+
+        private static bool TestWithThree(int input)
+            {
+            return (input % 3 == 0);
+            }
+
+        private static bool TestBoth(int input)
+            {
+            return (input % 3 == 0 && input % 5 == 0);
+            }
+        }
+    }
---
+++ TDD-Kata/TDD-Kata/FizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDD_Kata.FizzBuzz
+    {
+    [TestFixture]
+    public class Runner
+        {
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [Test]
+        public void Run(int input, string actual)
+            {
+            var result = Problem.FizzBuzz(input);
+            Assert.That(result, Is.EqualTo(actual));
+            }
+        }
+    }
